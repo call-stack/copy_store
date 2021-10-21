6ae889f715aa8030c501672a7f6cd32f50d9355a
kalpitpant <kalpitpant32@gmail.com>
2021-10-21 18:02:36 +0530

    Refactor

--- internal/redis/core.go
+++ internal/redis/core.go
@@ -1,18 +1,44 @@
 package redis
 
 import (
+	"context"
+	"errors"
+
 	"github.com/go-redis/redis/v8"
 )
 
-type RedisCore struct {
+type RedisDB struct {
+	Client *redis.Client
 }
 
-var Client *redis.Client
+var RedisClient *RedisDB
 
-func (r *RedisCore) SetRedisClient() {
-	Client = redis.NewClient(&redis.Options{
+var (
+	errNil = errors.New("no matching record found in redis database.")
+	ctx    = context.TODO()
+)
+
+func NewDatabase(address string) (*RedisDB, error) {
+	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "",
 		DB:       0,
 	})
+
+	if err := client.Ping(ctx).Err(); err != nil {
+		return nil, err
+	}
+
+	return &RedisDB{
+		Client: client,
+	}, nil
+
 }
+
+// func SetClient() {
+// 	RedisClient, _ = newDatabase("localhost:6379")
+// }
+
+// func GetClient() *RedisDB {
+// 	return RedisClient
+// }
--- internal/store/core.go
+++ internal/store/core.go
@@ -5,18 +5,19 @@ import (
 	"fmt"
 
 	"github.com/call-stack/copy_store.git/rpc/copystore"
+	pb "github.com/call-stack/copy_store.git/rpc/copystore"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
-type StoreCore struct{}
+type CopyStore struct{}
 
-func (sc *StoreCore) StoreContent(ctx context.Context, req *copystore.PasteReq) (*copystore.PasteResp, error) {
+func (s *CopyStore) PasteContent(ctx context.Context, req *pb.PasteReq) (*pb.PasteResp, error) {
 	val := &Validator{}
 	if err := val.ValidatePasteReq(req); err != nil {
 		return &copystore.PasteResp{}, nil
 	}
-	client_ip := ctx.Value("remote-addr").(string)
 
+	client_ip := ctx.Value("remote-addr").(string)
 	h := &Helper{}
 	hash := h.getUniqueHash(client_ip)
 	b, _ := h.isHashPresentInRedis(ctx, hash)
@@ -24,10 +25,15 @@ func (sc *StoreCore) StoreContent(ctx context.Context, req *copystore.PasteReq) 
 		h.setHashInRedis(ctx, hash)
 
 	}
+
 	content_url := fmt.Sprintf("http://127.0.0.1:8080/twirp/GetContent/%s", hash)
 	content := DataStore{URL: content_url, Content: req.Content}
 	bson_content, _ := bson.Marshal(content)
 	h.storeInMongo(ctx, bson_content)
 
 	return &copystore.PasteResp{Url: content_url}, nil
+}
+
+func (s *CopyStore) GetContent(ctx context.Context, req *pb.GetReq) (*pb.GetResp, error) {
+	return nil, nil
 }
